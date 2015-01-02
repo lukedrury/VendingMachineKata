2fa4dd23badf762990930f375ed0bc8a62a272f6
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-02 16:31:06 +0000

    Add buttons 2 - 5

--- VendingMachine/ButtonPad.cs
+++ VendingMachine/ButtonPad.cs
@@ -6,6 +6,11 @@ namespace VendingMachine
     {
         private readonly Selection m_Selection = new Selection();
         public readonly IButton One;
+        public readonly IButton Two;
+        public readonly IButton Three;
+        public readonly IButton Four;
+        public readonly IButton Five;
+        public readonly IButton Six;
 
         public interface IButton
         {
@@ -30,6 +35,11 @@ namespace VendingMachine
         public ButtonPad()
         {
             One = new Button(() => m_Selection.AddToSelection(1));
+            Two = new Button(() => m_Selection.AddToSelection(2));
+            Three = new Button(() => m_Selection.AddToSelection(3));
+            Four = new Button(() => m_Selection.AddToSelection(4));
+            Five = new Button(() => m_Selection.AddToSelection(5));
+            Six = new Button(() => m_Selection.AddToSelection(6));
         }
 
         public string GetCurrentSelection()
--- VendingMachine/ButtonPadTests.cs
+++ VendingMachine/ButtonPadTests.cs
@@ -1,5 +1,4 @@
-﻿using System.CodeDom;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace VendingMachine
 {
@@ -13,12 +12,48 @@ namespace VendingMachine
         }
 
         [Test]
-        public void OneButtonPressShouldShowInSelection()
+        public void ButtonOneShouldShowInSelection()
         {
             var buttonPad = new ButtonPad();
             buttonPad.One.Press();
-            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo("1"));
-            
+            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo("1"));            
         }
+
+        [Test]
+        public void ButtonTwoShouldShowInSelection()
+        {
+            var buttonPad = new ButtonPad();
+            buttonPad.Two.Press();
+            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo("2"));
+        }
+        [Test]
+        public void ButtonThreeShouldShowInSelection()
+        {
+            var buttonPad = new ButtonPad();
+            buttonPad.Three.Press();
+            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo("3"));
+        }
+        [Test]
+        public void ButtonFourShouldShowInSelection()
+        {
+            var buttonPad = new ButtonPad();
+            buttonPad.Four.Press();
+            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo("4"));
+        }
+        [Test]
+        public void ButtonFiveShouldShowInSelection()
+        {
+            var buttonPad = new ButtonPad();
+            buttonPad.Five.Press();
+            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo("5"));
+        }
+        [Test]
+        public void ButtonSixShouldShowInSelection()
+        {
+            var buttonPad = new ButtonPad();
+            buttonPad.Six.Press();
+            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo("6"));
+        }
+
     }
 }
