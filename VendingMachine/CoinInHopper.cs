e4669dcb07c8a54185859fc1a87e1e65f04756d7
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-02 17:21:29 +0000

    Add a test for inserting a single coin into the hopper

--- VendingMachine/CoinInHopper.cs
+++ VendingMachine/CoinInHopper.cs
@@ -1,16 +1,19 @@
-﻿using System.Dynamic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace VendingMachine
 {
     class CoinInHopper
     {
+        private readonly List<ICoin> m_Coins = new List<ICoin>();
         public void Insert(ICoin coin)
         {
+            m_Coins.Add(coin);
         }
 
         public int TotalInserted
         {
-            get { return 0; }
+            get { return m_Coins.Sum(coin => coin.Value); }
         }
     }
 }
--- VendingMachine/CoinInHopperTests.cs
+++ VendingMachine/CoinInHopperTests.cs
@@ -10,5 +10,13 @@ namespace VendingMachine
             var hopper = new CoinInHopper();
             Assert.That(hopper.TotalInserted, Is.EqualTo(0));
         }
+        
+        [Test]
+        public void OneCoinShouldShowInTotal()
+        {
+            var hopper = new CoinInHopper();
+            hopper.Insert(Coin.New5p());
+            Assert.That(hopper.TotalInserted, Is.EqualTo(5));
+        }
     }
 }
