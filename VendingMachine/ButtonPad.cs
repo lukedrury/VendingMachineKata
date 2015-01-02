40dd5ac35a76a3edac5369a7858c38c758e95663
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-02 15:28:07 +0000

    First passing test - a button pad that has nothing selected when it starts up

---
+++ VendingMachine/ButtonPad.cs
@@ -0,0 +1,10 @@
+﻿namespace VendingMachine
+{
+    public class ButtonPad
+    {
+        public string GetCurrentSelection()
+        {
+            return "";
+        }
+    }
+}
---
+++ VendingMachine/ButtonPadTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace VendingMachine
+{
+
+    public class ButtonPadTests
+    {
+        [Test]
+        public void ShouldStartWithNoSelection()
+        {
+            var buttonPad = new ButtonPad();
+            Assert.That(buttonPad.GetCurrentSelection(), Is.EqualTo(""));
+        }
+    }
+}
