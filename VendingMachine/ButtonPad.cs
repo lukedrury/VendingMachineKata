e273613b4652719f37b00ffa4195741cba813f01
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-02 16:44:23 +0000

    Simply handling of the clearTransaction action

--- VendingMachine/ButtonPad.cs
+++ VendingMachine/ButtonPad.cs
@@ -4,7 +4,6 @@ namespace VendingMachine
 {
     public class ButtonPad
     {
-        private readonly Action m_ClearTransaction;
         private readonly Selection m_Selection = new Selection();
         public readonly IButton One;
         public readonly IButton Two;
@@ -17,7 +16,6 @@ namespace VendingMachine
 
         public ButtonPad(Action clearTransaction)
         {
-            m_ClearTransaction = clearTransaction;
             One = new Button(() => m_Selection.AddToSelection(1));
             Two = new Button(() => m_Selection.AddToSelection(2));
             Three = new Button(() => m_Selection.AddToSelection(3));
@@ -25,7 +23,7 @@ namespace VendingMachine
             Five = new Button(() => m_Selection.AddToSelection(5));
             Six = new Button(() => m_Selection.AddToSelection(6));
             ClearSelection = new Button(() => m_Selection.Clear());
-            ClearTransaction = new Button(() => m_ClearTransaction());
+            ClearTransaction = new Button(clearTransaction);
         }
 
         public string GetCurrentSelection()
